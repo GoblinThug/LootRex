69e13c9c6cb07136a8b8cee2923624b562058e1b
GoblinThug <goblin.thug@mail.ru>
2025-07-02 18:20:27 +0600

    NEW: Main Page (Header, Footer, Main components)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script setup>
-
+	import Header from "@/components/Header.vue";
+	import LiveFeed from "@/components/LiveFeed.vue";
+	import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
-	<div class="h-full flex flex-col">
-		<div class="flex-">
-
+	<div class="h-full flex flex-col gap-10">
+		<div class="flex flex-col gap-1">
+			<Header/>
+			<LiveFeed/>
 		</div>
-		<router-view/>
-		<div>
-
+		<div class="px-2">
+			<router-view/>
 		</div>
+		<Footer/>
 	</div>
 </template>
 
